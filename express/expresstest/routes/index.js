253c61600ee9868c5b72be62ba1dcfd1a94036b7
Natasha Rios <natu.rios@hotmail.com>
2020-12-03 18:48:41 -0300

    ejercicio

--- express/expresstest/routes/index.js
+++ express/expresstest/routes/index.js
@@ -6,8 +6,19 @@ router.get('/users/:name', function(req, res, next) {
 });
 
 router.get('/multiplication/:number', function (req, res, next) {
-  const number = Number(req.params.number * 2)
+  const number = Number(req.params.number * 2);
   res.send(`${number}`);
+});
+
+router.get('/palindromo/:word', function (req, res, next) {
+  let word = req.params.word.toLowerCase();
+  let palindromo = word.split('').reverse().join('');
+
+  if(word == palindromo){
+    res.send(`${req.params.word} es palíndromo ya que al revés se lee ${palindromo}`)
+  }else{
+    res.send(`${req.params.word} NO es palíndromo ya que al revés se lee ${palindromo}`)
+  }
 });
 
 /* GET home page. */
